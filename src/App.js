baed009cee642dc9713769503be3703f40e54528
Isabella Pegoraro Marcondes <isabella.pegorarom@gmail.com>
2021-01-13 09:24:56 -0300

    eslintcache

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ const App = () => {
         </Route>
         <Route path="/register">
           <RegisterUser />
-          {/* <ResponsibleForm /> */}
         </Route>
       </Switch>
     </>
