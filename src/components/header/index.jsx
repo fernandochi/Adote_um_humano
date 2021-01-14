334bb9cf837017f14802f896e6899bf37936a3b5
Isabella Pegoraro Marcondes <isabella.pegorarom@gmail.com>
2021-01-14 20:11:25 -0300

    feat: adding change page logic

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,23 +1,41 @@
 import Logo from "../logo/index";
 import { Container, ContainerButtons, Button } from "./style";
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 
 const Header = () => {
   const location = useLocation();
+  const history = useHistory();
   return (
     <Container>
-      <span>Menu</span>
-      <ContainerButtons>
-        <Button location={location.pathname} path="/">
-          Home
-        </Button>
-        <Button location={location.pathname} path="login">
-          Login
-        </Button>
-        <Button location={location.pathname} path="register">
-          Cadastro
-        </Button>
-      </ContainerButtons>
+      {location.pathname === "/" && (
+        <>
+          <span>Menu</span>
+          <ContainerButtons>
+            <Button
+              onClick={() => history.push("/")}
+              location={location.pathname}
+              path="/"
+            >
+              Home
+            </Button>
+            <Button
+              onClick={() => history.push("/login")}
+              location={location.pathname}
+              path="login"
+            >
+              Login
+            </Button>
+            <Button
+              onClick={() => history.push("/register")}
+              location={location.pathname}
+              path="register"
+            >
+              Cadastro
+            </Button>
+          </ContainerButtons>{" "}
+        </>
+      )}
+
       <Logo />
     </Container>
   );
