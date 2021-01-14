d033b0c374cca2bb3ceec16861ca2e05150e3844
Isabella Pegoraro Marcondes <isabella.pegorarom@gmail.com>
2021-01-14 16:09:08 -0300

    feat: adding logo and style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Switch } from "react-router-dom";
 import RegisterUser from "./pages/Register";
 import LoginUser from "./pages/Login";
+import Logo from "./components/logo/index";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Switch>
         <Route exact path="/">
           <h1>awasome app</h1>
+          <Logo />
         </Route>
 
         <Route exact path="/login">
---
+++ src/components/logo/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Text = styled.h1`
+  font-family: "Amatic SC";
+  font-weight: bolder;
+  font-size: 30px;
+
+  @media (min-width: 700px) {
+    font-size: 60px;
+  }
+`;
+
+export { Text };
