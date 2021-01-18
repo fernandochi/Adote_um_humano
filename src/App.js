674a4b04e4d556ebfa3ce087878cf3a0fffd1c6a
Isabella Pegoraro Marcondes <isabella.pegorarom@gmail.com>
2021-01-18 19:25:17 -0300

    fix: fixing conflict

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
 import { Route, Switch } from "react-router-dom";
 import RegisterUser from "./pages/Register";
 import LoginUser from "./pages/Login";
+import Animals from "./pages/Animals";
 import Header from "./components/header/index";
 import Footer from "./components/footer/index";
 import RegisterAnimal from "./pages/AnimalForm";
 import Error404 from "./pages/Error404";
-import AdopterProfile from "./pages/AdopterProfile";
-import CardSecondary from "./components/CardSecondary";
-import imgTest from "./assets/img/306277-min.png";
+import AdopterProfile from "./pages/AdopterProfile/index";
 
 const App = () => {
   return (
     <>
+      <Header />
       <Switch>
-        <Route exact path="/">
-          <Header />
-          <CardSecondary
-            img={imgTest}
-            name="Viktor Fagioanto"
-            email="viktor@gmail.com"
-            phone="(11) 94729-2091"
-            address="Rua Logo Ali, 150 - San Andreas - SP"
-            responsiveForm
-          />
-        </Route>
+        <Route exact path="/"></Route>
 
         <Route exact path="/login">
           <LoginUser />
@@ -31,10 +21,12 @@ const App = () => {
         <Route path="/register">
           <RegisterUser />
         </Route>
+
+        <Route path="/animals" component={Animals} />
         <Route path="/animal-form">
           <RegisterAnimal />
         </Route>
-        <Route path="/adopter">
+        <Route path="adopter">
           <AdopterProfile />
         </Route>
         <Route>
