661222da49a89d1ed082d32191d71e1e7fc9b7d3
Isabella Pegoraro Marcondes <isabella.pegorarom@gmail.com>
2021-01-18 19:10:48 -0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import { Route, Switch } from "react-router-dom";
 import RegisterUser from "./pages/Register";
 import LoginUser from "./pages/Login";
-import Animals from "./pages/Animals";
 import Header from "./components/header/index";
 import Footer from "./components/footer/index";
 import RegisterAnimal from "./pages/AnimalForm";
 import Error404 from "./pages/Error404";
-<<<<<<< HEAD
 import AdopterProfile from "./pages/AdopterProfile";
-=======
 import CardSecondary from "./components/CardSecondary";
 import imgTest from "./assets/img/306277-min.png";
->>>>>>> c4a2a9e91d2ce2b832d32a41a14b33a2be4947d2
 
 const App = () => {
   return (
     <>
-      <Header />
       <Switch>
-<<<<<<< HEAD
         <Route exact path="/">
           <Header />
           <CardSecondary
@@ -30,9 +24,6 @@ const App = () => {
             responsiveForm
           />
         </Route>
-=======
-        <Route exact path="/"></Route>
->>>>>>> b928e001ba71f31e1a5f8831786f2ef373640ebb
 
         <Route exact path="/login">
           <LoginUser />
@@ -40,8 +31,6 @@ const App = () => {
         <Route path="/register">
           <RegisterUser />
         </Route>
-
-        <Route path="/animals" component={Animals} />
         <Route path="/animal-form">
           <RegisterAnimal />
         </Route>
